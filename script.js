8b4563889a4e2d29ce1c45a8fbe9a2f7586ac60a
shaikanees19 <aneesahamed.official@gmail.com>
2026-03-26 22:08:43 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+function loadData() {
+  fetch('output.json?t=' + new Date().getTime())
+    .then(response => response.json())
+    .then(data => {
+
+      document.getElementById("stress").innerText =
+        "Stress Level: " + data.stress_label;
+
+      document.getElementById("percentage").innerText =
+        "Stress Percentage: " + data.stress_percentage + "%";
+
+      document.getElementById("alpha").innerText =
+        "Alpha: " + data.relative_alpha;
+
+      document.getElementById("beta").innerText =
+        "Beta: " + data.relative_beta;
+
+      document.getElementById("theta").innerText =
+        "Theta: " + data.relative_theta;
+
+      document.getElementById("time").innerText =
+        "Last Updated: " + data.timestamp;
+    });
+}
+
+loadData();
+setInterval(loadData, 5000);
